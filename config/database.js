6846ff47593f03707b2d75052a046956a34e9dce
sandeep_danta <sandeep@babajob.com>
2014-10-02 01:36:55 +0530

    Added room functionality

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url' : 'mongodb://chatsy:tsy381@proximus.modulusmongo.net:27017/arodI3ba?autoReconnect=true&connectTimeoutMS=60000'
+	'url' : 'mongodb://chatsy:tsy381@proximus.modulusmongo.net:27017/arodI3ba?autoReconnect=true&connectTimeoutMS=360000'
 };
