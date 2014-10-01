757e1251edcdc8e43aa8b31375535a5f6ce8f43f
devmaestro <sandeep.danta@gmail.com>
2014-10-02 01:36:55 +0530

    Added room functionality

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,20 @@
 var Groups = require('../app/models/Groups');
 
-module.exports = function(server) {
+module.exports = function (server)
+{
 	//GET routes
 	//general
 	server.route({
 		method: 'GET',
 		path: '/',
-		handler: function(request, reply) {
-			if(request.state['alias'] == undefined) {
+		handler: function (request, reply)
+		{
+			if (request.state['alias'] == undefined)
+			{
 				//User not logged in
 				reply.file('./views/index.html');
-			} else {
+			} else
+			{
 				//User logged in
 				reply.file('./views/home.html');
 			}
@@ -19,11 +23,14 @@ module.exports = function(server) {
 	server.route({
 		method: 'GET',
 		path: '/home',
-		handler: function(request, reply) {
-			if(request.state['alias'] == undefined) {
+		handler: function (request, reply)
+		{
+			if (request.state['alias'] == undefined)
+			{
 				//User not logged in
 				reply.file('./views/index.html');
-			} else {
+			} else
+			{
 				//User logged in
 				reply.file('./views/home.html');
 			}
@@ -32,87 +39,115 @@ module.exports = function(server) {
 	server.route({
 		method: 'GET',
 		path: '/groups',
-		handler: function(request, reply) {
-			if(request.state['alias'] == undefined) {
+		handler: function (request, reply)
+		{
+			if (request.state['alias'] == undefined)
+			{
 				//User not logged in
 				reply.file('./views/index.html');
-			} else {
+			} else
+			{
 				//User logged in
 				var groups = {};
-				Groups.find({},function(err, users) {
-					reply(users);
+				Groups.find({}, function (err, groups)
+				{
+					reply(groups);
 				});
 			}
 		}
 	});
+	server.route(
+	{
+		method: 'GET',
+		path: '/groups/{id}',
+		handler: function (request, reply)
+		{
+			Groups.find({ _id: request.params.id }, function (err, result)
+			{
+				reply.view('chat', { name: result[0].name, groupId: result[0]._id });
+			});
+		}
+	});
+	
 	//resources
 	server.route({
 		method: 'GET',
 		path: '/semantic.css',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/semantic/build/packaged/css/semantic.css');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/semantic.min.css',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/semantic/build/packaged/css/semantic.min.css');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/semantic.js',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/semantic/build/packaged/javascript/semantic.js');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/semantic.min.js',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/semantic/build/packaged/javascript/semantic.min.js');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/chatsy.css',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./public/stylesheets/chatsy.css');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/jquery.js',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/jquery/dist/jquery.js');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/jquery.min.js',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/jquery/dist/jquery.min.js');
 		}
 	});
 	server.route({
 		method: 'GET',
 		path: '/jquery.min.map',
-		handler: function(request, reply) {
+		handler: function (request, reply)
+		{
 			reply.file('./bower_components/jquery/dist/jquery.min.map');
 		}
 	});
-	
+
 	//-----------------------------------------------------------------------
 	//POST routes
 	//general
 	server.route({
 		method: 'POST',
 		path: '/',
-		handler: function(request, reply) {
-			if(request.payload.alias != undefined) {
-				reply("Success").state('alias',request.payload.alias);
-			} else {
+		handler: function (request, reply)
+		{
+			if (request.payload.alias != undefined)
+			{
+				reply("Success").state('alias', request.payload.alias);
+			} else
+			{
 				reply("Error");
 			}
 		}
@@ -120,24 +155,30 @@ module.exports = function(server) {
 	server.route({
 		method: 'POST',
 		path: '/groups/create',
-		handler: function(request, reply) {
-			if(request.payload.alias != undefined) {
+		handler: function (request, reply)
+		{
+			if (request.payload.alias != undefined)
+			{
 				var newGroup = new Groups();
 				newGroup.name = request.payload.name;
 				newGroup.description = request.payload.description;
 				newGroup.key = newGroup.generateHash(request.payload.key);
 				newGroup.createdBy = request.payload.alias;
-				newGroup.save(function(err) {
-					if(err) {
+				newGroup.save(function (err)
+				{
+					if (err)
+					{
 						reply(err.message);
-					} else {
+					} else
+					{
 						reply("Success");
 					}
-				});				
-			} else {
+				});
+			} else
+			{
 				reply("Error");
 			}
 		}
 	});
-	
+
 };
--- server.js
+++ server.js
@@ -1,16 +1,56 @@
 //Require Module Dependencies
 var hapi = require('hapi'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	Path =  require('path'),
+	socketio = require('socket.io');
 //Require Imports
 var routes = require('./app/routes'),
 	configDB = require('./config/database.js');
 
 mongoose.connect(configDB.url);
 mongoose.connection.on('disconnected', function() { console.log("disconnected from db...");});
-var server = hapi.createServer('localhost', 1243);
+
+var serverOptions = 
+{
+	views : 
+	{
+		engines : 
+		{
+			html : require('handlebars')			
+		},
+		basePath : __dirname,
+		path : './views'
+	}
+}
+var host = 'localhost';
+var port = 1000;
+var server = hapi.createServer('localhost', port, serverOptions);
 routes(server);
-server.start(function() {
-	console.log('Server started on port 3000');
+
+var groups = {};
+server.start(function ()
+{
+	var io = socketio.listen(server.listener);
+	io.on('connection', function (socket)
+	{
+		socket.on('join', function (groupId)
+		{
+			if (groups[groupId] == undefined)
+			{
+				groups[groupId] = groupId;
+			}
+			socket.join(groupId, function ()
+			{
+				
+			});
+		});
+		socket.on('send message', function (data)
+		{
+			io.sockets.in(data.groupId).emit('send message', { 'message': data.message, 'sender': data.sender });			
+		})
+	});
+
+	console.log('Server started on port: ' + port);
 });
 
 
