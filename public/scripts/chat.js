20493bc2a39d1e6b55c4727dc4fcc598cf108271
sandeep_danta <sandeep@babajob.com>
2014-10-30 21:40:45 +0530

    Experiments on chat page: -Card layout in chats

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -14,18 +14,22 @@ socket.on('connect', function ()
 
 socket.on('send message', function (data)
 {
-	$('#chat-message').append('<div class=\"ui grid\"><div class=\"two wide column\" style=\"font-weight:bold\">' + data.sender + ':</div><div class=\"fourteen wide column\">' + data.message.replace('\n', '<br/>') + '</div></div>');
+	var messageContainer = $('#chat-message');
+	messageContainer.append('<div style="margin-top: 5px; background-color: #efefef; box-shadow: 1px 1px 1px 1px gray;padding: 10px"><div style="font-weight:bold; display: inline-block">' + data.sender + ':</div><div style="display: inline-block; word-break: break-all">' + data.message.replace('\n', '<br/>') + '</div></div>');
+	messageContainer.scrollTop(messageContainer.prop("scrollHeight"));
 });
 socket.on('send image message', function (data)
 {
-	$('#chat-message').append('<div class=\"ui grid\"><div class=\"two wide column\" style=\"font-weight:bold\">' + data.sender + ':</div><div class=\"fourteen wide column\"><img style=\"max-width: 25%\" src=\"' + data.message + '\"/></div></div>');
+	var messageContainer = $('#chat-message');
+	messageContainer.append('<div style="margin-top: 5px; background-color: #efefef; box-shadow: 1px 1px 1px 1px gray;padding: 10px"><div style="font-weight:bold; display: inline-block">' + data.sender + ':</div><div style="display: inline-block; word-break: break-all"><img src=\"' + data.message + '"/></div></div>');
+	messageContainer.scrollTop(messageContainer.prop("scrollHeight"));
 });
 socket.on('users update', function (data)
 {
 	$('#chat-people').empty();
 	for (var i = 0; i < data.groups[groupId].length; i++)
 	{
-		$('#chat-people').append('<div>' + data.groups[groupId][i] + '</div>');
+		$('#chat-people').append('<div style="padding: 10px; background-color: #efefef; margin-top: 5px">' + data.groups[groupId][i] + '</div>');
 	}	
 });
 $('#chat-input').keydown(function (e)
@@ -56,6 +60,12 @@ window.onbeforeunload = function (e)
 {
 	socket.emit('leave group', { 'groupId': groupId, 'alias': cookies['alias'] });
 };
+
+$(document).ready(function(e)
+{
+	var messageContainer = $('#chat-message');
+	messageContainer.scrollTop(messageContainer.prop("scrollHeight"));
+});
 
 function str_obj(str)
 {
